474ae9a462deb5c1dc52330222ea4bb898bef71f
yapplejack <jacklf@umich.edu>
2026-01-31 23:28:18 -0500

    minor spelling

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,7 +16,7 @@ import com.revrobotics.spark.config.SparkFlexConfig;
  *
  * <p>
  * It is advised to statically import this class (or one of its inner classes)
- * wherever the constants are needed, to reduce verbosity.
+ * wherever the constants are needed, to reduce verbosity. 
  */
 public final class Constants {
 
